9fc7c838e98a04944d83e641b8f90fdd5d9ccd48
Lilian <lilian.kojan@gmail.com>
2021-05-26 18:00:28 +0200

    added further yolo documentation

--- model_yolo-farming/03_experiments_yolo-farming-social.jl
+++ model_yolo-farming/03_experiments_yolo-farming-social.jl
@@ -2,6 +2,7 @@ using Agents
 using Plots
 using Statistics
 using Arrow
+using Measures
 #using AgentsPlots
 
 # Include functions
@@ -11,23 +12,25 @@ include("helpers_yolo-farming.jl")
 agentdata = [:ccexperience, :ppexperience, :ccbelief, :ccrisk, :ppintention,
 :participation]
 
+
 # Experiment 1
 # For the first experiment, we use the default conditions.
 # The model runs for 10 years, from 2010 to 2020
 # Initialize model
 model1 = initialize()
 # collect data
-adata, mdata = run!(model1, agent_step!, model_step!, agents_first=false, 10,
-adata = agentdata, mdata = [:stepcounter], replicates = 100)
+adata1, mdata1 = run!(model1, agent_step!, model_step!, agents_first=false, 10,
+adata = agentdata, mdata = [:stepcounter], replicates = 500)
 # create summary data
-summarydata1 = sumdata(adata, mdata)
+summarydata1 = sumdata(adata1, mdata1)
 # save summary data
 Arrow.write("model_yolo-farming/data-output/experiment-1_summary.arrow", summarydata1)
 # plot data
 plotgraph(summarydata1)
 plot!(title = "Experiment 1")
 # save plot
-png("model_yolo-farming/data-output/experiment-1_figure.png")
+savefig("model_yolo-farming/data-output/experiment-1_figure.svg")
+savefig("model_yolo-farming/data-output/experiment-1_figure.pdf")
 
 # Experiment 2
 # For the second experiment, we set the social threshold to 1.
@@ -35,19 +38,20 @@ png("model_yolo-farming/data-output/experiment-1_figure.png")
 # influenced socially to participate.
 # The model runs for 10 years, from 2010 to 2020
 # Initialize model
-model2 = initialize(; socialthreshold = 0.75)
+model2 = initialize(; socialthreshold = 1)
 # collect data
-adata, _ = run!(model2, agent_step!, model_step!, agents_first=false, 10,
+adata2, mdata2 = run!(model2, agent_step!, model_step!, agents_first=false, 10,
 adata = agentdata, mdata = [:stepcounter], replicates = 100)
 # create summary data
-summarydata2 = sumdata(adata, mdata)
+summarydata2 = sumdata(adata2, mdata2)
 # save summary data
-Arrow.write("model_yolo-farming/data-output/experiment-2_summary.arrow", summarydata1)
+Arrow.write("model_yolo-farming/data-output/experiment-2_summary.arrow", summarydata2)
 # plot data
 plotgraph(summarydata2)
 plot!(title = "Experiment 2")
 # save plot
-png("model_yolo-farming/data-output/experiment-2_figure.png")
+savefig("model_yolo-farming/data-output/experiment-2_figure.svg")
+savefig("model_yolo-farming/data-output/experiment-2_figure.pdf")
 
 # Experiment 3
 # For the third experiment, we set the initial number of program participants
@@ -55,33 +59,48 @@ png("model_yolo-farming/data-output/experiment-2_figure.png")
 # Initialize model
 model3 = initialize(; participatefract = 0.5)
 # collect data
-adata, _ = run!(model3, agent_step!, model_step!, agents_first=false, 10,
+adata3, mdata3 = run!(model3, agent_step!, model_step!, agents_first=false, 10,
 adata = agentdata, mdata = [:stepcounter], replicates = 100)
 # create summary data
-summarydata3 = sumdata(adata, mdata)
+summarydata3 = sumdata(adata3, mdata3)
 # save summary data
-Arrow.write("model_yolo-farming/data-output/experiment-3_summary.arrow", summarydata1)
+Arrow.write("model_yolo-farming/data-output/experiment-3_summary.arrow", summarydata3)
 # plot data
 # plot data
 plotgraph(summarydata3)
 plot!(title = "Experiment 3")
 # save plot
-png("model_yolo-farming/data-output/experiment-3_figure.png")
+savefig("model_yolo-farming/data-output/experiment-3_figure.svg")
+savefig("model_yolo-farming/data-output/experiment-3_figure.pdf")
 
 # Experiment 4
 # For the third experiment, we set the program quality to -0.5.
 # The model runs for 10 years, from 2010 to 2020
 # Initialize model
-model4 = initialize(; programquality = -0.5)
+model4 = initialize(; programquality = -0.4)
 # collect data
-adata, _ = run!(model4, agent_step!, model_step!, agents_first=false, 10,
+adata4, mdata4 = run!(model4, agent_step!, model_step!, agents_first=false, 10,
 adata = agentdata, mdata = [:stepcounter], replicates = 100)
 # create summary data
-summarydata4 = sumdata(adata, mdata)
+summarydata4 = sumdata(adata4, mdata4)
 # save summary data
-Arrow.write("model_yolo-farming/data-output/experiment-4_summary.arrow", summarydata1)
+Arrow.write("model_yolo-farming/data-output/experiment-4_summary.arrow", summarydata4)
 # plot data
 plotgraph(summarydata4)
 plot!(title = "Experiment 4")
 # save plot
-png("model_yolo-farming/data-output/experiment-4_figure.png")
+savefig("model_yolo-farming/data-output/experiment-4_figure.svg")
+savefig("model_yolo-farming/data-output/experiment-4_figure.pdf")
+
+
+# Make summary graph
+sdatalist = [summarydata1, summarydata2, summarydata3, summarydata4]
+
+plot(plotsumgraph(summarydata1), plotsumgraph(summarydata2),
+plotsumgraph(summarydata3), plotsumgraph(summarydata4),
+title = ["Experiment 1" "Experiment 2" "Experiment 3" "Experiment 4"],
+size = (1000,750), margin = 5mm)
+
+#save plot
+savefig("model_yolo-farming/data-output/experiments-summary_figure.svg")
+savefig("model_yolo-farming/data-output/experiments-summary_figure.pdf")
--- model_yolo-farming/helpers_yolo-farming.jl
+++ model_yolo-farming/helpers_yolo-farming.jl
@@ -5,7 +5,7 @@ function sumdata(adata, mdata)
                 :ccexperience => std, :ppexperience => mean,
                 :ppexperience => std, :ccbelief => mean, :ccbelief => std,
                 :ccrisk => mean, :ccrisk => std, :ppintention => mean,
-                :ppintention=> std, :participation => mean,
+                :ppintention=> std, :participation => mean, :participation => std,
                 :ppexperience => (x -> quantile(x, 0.25)) => :ppexperiencelower,
                 :ppexperience => (x -> quantile(x, 0.75)) => :ppexperienceupper,
                 :ccexperience => (x -> quantile(x, 0.25)) => :ccexperiencelower,
@@ -21,7 +21,32 @@ end
 function plotgraph(summarydata)
     plot(summarydata.year, summarydata.wateravailability,
     label = "Water availability", linecolor = "#0077BB", ylims = [-1,1],
-    legend = :outerbottommiddle, xlabel = "Years = steps")
+    legend = :bottom,
+    # dpi = 300,
+    xlabel = "Years = steps")
+    plot!(summarydata.year, summarydata.ppexperience_mean, linecolor = "#EE3377",
+    label = "Policy experience mean")
+    plot!(summarydata.year, summarydata.ppexperiencelower, linealpha = 0,
+    fillrange = summarydata.ppexperienceupper, fillcolor ="#EE3377",
+    fillalpha = 0.15, label = "Policy experience Q2&Q3")
+    plot!(summarydata.year, summarydata.ccexperience_mean, linecolor = "#EE7733",
+    label = "Climate change experience mean")
+    plot!(summarydata.year, summarydata.ccexperiencelower, linealpha = 0,
+    fillrange = summarydata.ccexperienceupper, fillcolor ="#EE7733",
+    fillalpha = 0.15, label = "Climate change experience Q2&Q3")
+    plot!(summarydata.year, summarydata.participation_mean, linecolor = "#009988",
+    fillalpha = 0.15, label = "Participating fraction mean")
+end
+
+xticks = ["2010" "" "" "" "" "2015" "" "" "" "" "2020"]
+function plotsumgraph(summarydata)
+    # for i in 1:length(arraylist)
+    plot(summarydata.year, summarydata.wateravailability,
+    label = "Water availability", linecolor = "#0077BB", ylims = [-1,1],
+    legend = :bottomright,
+    # dpi = 300,
+    xlabel = "Years = steps",
+    xticks = (1:10, xticks))
     plot!(summarydata.year, summarydata.ppexperience_mean, linecolor = "#EE3377",
     label = "Policy experience mean")
     plot!(summarydata.year, summarydata.ppexperiencelower, linealpha = 0,
